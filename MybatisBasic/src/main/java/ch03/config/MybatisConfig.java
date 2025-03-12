b5da71384c5d5aa2f2b85eafaa7869a384c344da
HSH-11 <lyk1517@naver.com>
2025-03-12 17:57:26 +0900

    delete pwd

--- MybatisBasic/src/main/java/ch03/config/MybatisConfig.java
+++ MybatisBasic/src/main/java/ch03/config/MybatisConfig.java
@@ -14,7 +14,7 @@ public class MybatisConfig {
 	        dataSource.setDriver("com.mysql.cj.jdbc.Driver");
 	        dataSource.setUrl("jdbc:mysql://localhost:3306/mybatis_basic");
 	        dataSource.setUsername("root");
-	        dataSource.setPassword("dldydrb15@");
+	        dataSource.setPassword("");
 	        Configuration configuration = new Configuration();
 	        configuration.setEnvironment(new org.apache.ibatis.mapping.Environment("development",
 	                new JdbcTransactionFactory(), dataSource));
